a708e7667dd4f5588097a8c624690983406069c5
mgallet <mgallet@spibol.19pouces.net>
2016-01-24 16:34:50 +0100

    cancel Debian 7

--- deb-ubuntu-14.04-15.10.sh
+++ deb-ubuntu-14.04-15.10.sh
@@ -5,7 +5,7 @@ set -e
 sudo apt-get update
 sudo apt-get upgrade --yes
 sudo apt-get install --yes vim dh-make ntp rsync liblzma-dev tree
-sudo apt-get install --yes python3-all-dev virtualenvwrapper python3-tz python3-setuptools python3-oauthlib apache2  libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap python-medusa python-meld3 ssl-cert python3-openid python3-msgpack
+sudo apt-get install --yes python3-all-dev virtualenvwrapper python3-tz python3-setuptools python3-oauthlib apache2  libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap python-medusa python-meld3 ssl-cert python3-msgpack
 sudo apt-get install --yes python3-gnupg libyaml-dev python3-yaml
 source /etc/bash_completion.d/virtualenvwrapper
 
