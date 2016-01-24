634e2495b7085358ed283c1982d20fcac9268ad1
mgallet <mgallet@spibol.19pouces.net>
2016-01-24 20:57:19 +0100

    start work on Debian 8

--- deb-debian-8.sh
+++ deb-debian-8.sh
@@ -5,8 +5,12 @@ set -e
 sudo apt-get update
 sudo apt-get upgrade --yes
 sudo apt-get install --yes vim dh-make ntp rsync liblzma-dev tree
-sudo apt-get install --yes python3-all-dev virtualenvwrapper python3-tz python3-setuptools apache2 apache2-mpm-worker apache2-utils apache2.2-bin apache2.2-common libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap python-medusa python-meld3 ssl-cert python3-openid
-sudo apt-get install --yes python3-gnupg libyaml-dev
+sudo apt-get install --yes python3-all-dev virtualenvwrapper \
+    python3-tz python3-setuptools \
+    python3-oauthlib \
+    apache2 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap \
+    python-medusa python-meld3 ssl-cert python3-msgpack
+sudo apt-get install --yes python3-gnupg libyaml-dev python3-yaml
 source /etc/bash_completion.d/virtualenvwrapper
 
 
@@ -44,7 +48,7 @@ sudo sed -i "s/localhost/$IP/g" /etc/moneta/settings.ini
 sudo a2ensite moneta.conf
 sudo a2dissite 000-default.conf
 sudo -u moneta moneta-manage migrate
-sudo service supervisor restart
+sudo service moneta-gunicorn start
 sudo service apache2 restart
 
 set -e
